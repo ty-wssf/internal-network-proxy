4005701ca0d7681615761c4d3c0b5fd9a33036cd
wuyilong <791128058@qq.com>
2024-06-30 15:04:05 +0800

    协议类型使用字典

--- network-proxy-dao/src/main/java/network/proxy/dao/_NetworkProxyDaoConstants.java
+++ network-proxy-dao/src/main/java/network/proxy/dao/_NetworkProxyDaoConstants.java
@@ -3,4 +3,14 @@ package network.proxy.dao;
 @SuppressWarnings({"PMD","java:S116"})
 public interface _NetworkProxyDaoConstants {
     
+    /**
+     * 协议类型: tcp 
+     */
+    int PROTOCOL_TYPE_TCP = 0;
+                    
+    /**
+     * 协议类型: http 
+     */
+    int PROTOCOL_TYPE_HTTP = 1;
+                    
 }
