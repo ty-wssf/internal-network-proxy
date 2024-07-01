311424d0902a7085b652b706a5d13cfb2075d6e8
wuyilong <791128058@qq.com>
2024-07-01 17:10:14 +0800

    代理客户端启用禁用功能开发

--- network-proxy-service/src/main/java/network/proxy/service/entity/ProxyClientsBizModel.java
+++ network-proxy-service/src/main/java/network/proxy/service/entity/ProxyClientsBizModel.java
@@ -2,13 +2,33 @@
 package network.proxy.service.entity;
 
 import io.nop.api.core.annotations.biz.BizModel;
+import io.nop.api.core.annotations.biz.BizMutation;
+import io.nop.api.core.annotations.core.Description;
+import io.nop.api.core.annotations.core.Name;
 import io.nop.biz.crud.CrudBizModel;
-
+import io.nop.core.context.IServiceContext;
 import network.proxy.dao.entity.ProxyClients;
 
 @BizModel("ProxyClients")
-public class ProxyClientsBizModel extends CrudBizModel<ProxyClients>{
-    public ProxyClientsBizModel(){
+public class ProxyClientsBizModel extends CrudBizModel<ProxyClients> {
+    public ProxyClientsBizModel() {
         setEntityName(ProxyClients.class.getName());
     }
+
+    @BizMutation
+    public boolean enabled(@Name("id") @Description("@i18n:biz.id|对象的主键标识") String id, IServiceContext context) {
+        ProxyClients entity = get(id, false, context);
+        entity.setEnabled(true);
+        dao().updateEntity(entity);
+        return true;
+    }
+
+    @BizMutation
+    public boolean disabled(@Name("id") @Description("@i18n:biz.id|对象的主键标识") String id, IServiceContext context) {
+        ProxyClients entity = get(id, false, context);
+        entity.setEnabled(false);
+        dao().updateEntity(entity);
+        return true;
+    }
+
 }
