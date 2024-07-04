5df1e94a54164c7d87f99e217d093246a5840aed
wuyilong <791128058@qq.com>
2024-07-04 20:19:53 +0800

    修改主题

--- network-proxy-app/src/main/java/network/proxy/app/NetworkProxyApplication.java
+++ network-proxy-app/src/main/java/network/proxy/app/NetworkProxyApplication.java
@@ -3,11 +3,22 @@ package network.proxy.app;
 
 
 import org.noear.solon.Solon;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Filter;
+import org.noear.solon.core.handle.FilterChain;
 
 public class NetworkProxyApplication {
 
     public static void main(String[] args) {
-        Solon.start(NetworkProxyApplication.class, args);
+        Solon.start(NetworkProxyApplication.class, args, solonApp -> {
+            solonApp.filter((ctx, chain) -> {
+                if ("/".equals(ctx.pathNew())) { //ContextPathFilter 就是类似原理实现的
+                    ctx.pathNew("/index.html");
+                }
+                chain.doFilter(ctx);
+            });
+
+        });
     }
 
 }
